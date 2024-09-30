f3eb160dacac2c9626ad36dd7be0143cc89fa717
Fabiano Souza <fabianokurashi@gmail.com>
2024-09-30 20:14:39 -0300

    feat: NavBar responsiva

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import styles from "./Navbar.module.css";
+import { getImageUrl } from '../../utils';
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    return (
+        <nav className={styles.navbar}>
+            <a className={styles.title} href='/'>Portfolio</a>
+            <div className={styles.menu}>
+                <img 
+                    className={styles.menuBtn}
+                    src={
+                        menuOpen
+                            ? getImageUrl("nav/closeIcon.png")
+                            : getImageUrl("nav/menuIcon.png")
+                    } 
+                    alt="botao-menu"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
+                <ul className={`${styles.menuItems} ${menuOpen ? styles.menuOpen : ''}`} onClick={() => setMenuOpen(false)}>
+                    <li><a href="#sobre">Sobre</a></li>
+                    <li><a href="#Experiencia">Experiência</a></li>
+                    <li><a href="#projetos">Projetos</a></li>
+                    <li><a href="#contato">Contato</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
